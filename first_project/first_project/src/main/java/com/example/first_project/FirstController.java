fd407ddbb24f67a74a485c7caaa7f0900935b0c9
hooni34 <choyh3502@gmail.com>
2026-03-25 14:51:30 +0900

    0325
    0325

---
+++ first_project/first_project/src/main/java/com/example/first_project/FirstController.java
@@ -0,0 +1,12 @@
+package com.example.first_project;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+    @GetMapping("/hi")
+    public String niceToMeetYou(){
+        return "greetings";
+    }
+}
